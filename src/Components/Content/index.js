f93cb250dec47ff5f8d30e02841d9245039acbe9
Stephan <obvilion@gmail.com>
2023-04-01 13:39:51 +0200

    Saving changes before reworking the entire app

--- src/Components/Content/index.js
+++ src/Components/Content/index.js
@@ -1,15 +1,17 @@
 import './index.scss';
 import LevelShare from "../Level_share";
 import DamageCalc from '../Damage_calc';
-import {Spacer, Text} from '@nextui-org/react';
+import AddChar from '../AddChar';
+import { Spacer, Text } from '@nextui-org/react';
 
 const Content = () => {
     return (
         <>
-            <Text h4 css={{textAlign: "center"}}>Party share range</Text>
-            <LevelShare/>
-            <Spacer y={4}/>
-            <DamageCalc/>
+            <AddChar />
+            {/* <Text h4 css={{textAlign: "center"}}>Party share range</Text> */}
+            {/* <LevelShare/> */}
+            {/* <Spacer y={4}/> */}
+            {/* <DamageCalc /> */}
         </>
     )
 }
--- src/Components/Damage_calc/index.js
+++ src/Components/Damage_calc/index.js
@@ -1,7 +1,7 @@
 import './index.scss';
-import {Container, Row, Text, Spacer, Collapse} from '@nextui-org/react';
-import {useContext} from 'react';
-import {DataContext} from "../../data/DataProvider";
+import { Container, Row, Text, Spacer, Collapse } from '@nextui-org/react';
+import { useContext } from 'react';
+import { DataContext } from "../../data/DataProvider";
 import RadioButtons from "../RadioButtons";
 import CharStats from "../CharStats";
 import Runes from "../Runes";
@@ -9,34 +9,34 @@ import VocationSpells from '../VocationSpells';
 import CapitalizeFirstLetter from "../Helpers/CapitalizeFirstLetter";
 
 const DamageCalc = () => {
-    const {data, checked, level, magiclvl, skill, attack} = useContext(DataContext);
+    const { data, checked, level, magiclvl, skill, attack } = useContext(DataContext);
 
 
     return data.length === 0 ? ('') : (
         <>
             <Container display="flex" justify="center">
-                <Text h4 css={{textAlign: "center"}}>Damage Calculator</Text>
+                <Text h4 css={{ textAlign: "center" }}>Damage Calculator</Text>
                 <Row justify="center">
                     {/*Select a vocation*/}
-                    <RadioButtons/>
-                    <Spacer y={4}/>
+                    <RadioButtons />
+                    <Spacer y={4} />
                 </Row>
                 {/*Insert character stats*/}
-                <CharStats/>
+                <CharStats />
                 {(checked === 'paladin' && level > 0 && magiclvl > 0 && skill > 0 && attack > 0) || (checked === 'knight' && level > 0 && magiclvl > 0 && skill > 0 && attack > 0) || (checked === 'sorcerer' && level > 0 && magiclvl > 0) || (checked === 'druid' && level > 0 && magiclvl > 0) ?
                     <>
-                        <Spacer y={1}/>
-                        <Text h4 css={{textAlign: "center"}}>These values are without powerful crit / forge upgrades for
+                        <Spacer y={1} />
+                        <Text h4 css={{ textAlign: "center" }}>These values are without powerful crit / forge upgrades for
                             now.</Text>
-                        <Spacer y={1}/>
+                        <Spacer y={1} />
                         <Collapse.Group>
                             {/*Runes*/}
                             <Collapse title="Runes">
-                                <Runes/>
+                                <Runes />
                             </Collapse>
                             {/*Voc spells*/}
                             <Collapse title={CapitalizeFirstLetter(checked) + ' spells'}>
-                                <VocationSpells/>
+                                <VocationSpells />
                             </Collapse>
                         </Collapse.Group>
                     </>
